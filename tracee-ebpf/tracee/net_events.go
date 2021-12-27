5b69649056c89b73ffc08540a1d4a5a0302a5963
ItamarMaouda <itamarmaouda101@gmail.com>
2021-12-27 10:08:47 +0200

    a

--- tracee-ebpf/tracee/net_events.go
+++ tracee-ebpf/tracee/net_events.go
@@ -10,7 +10,77 @@ import (
 	"inet.af/netaddr"
 )
 
-func DnsPaseName(payload []byte) string {
+type pktMeta struct {
+	SrcIP    [16]byte `json:"src_ip"`
+	DestIP   [16]byte `json:"dest_ip"`
+	SrcPort  uint16   `json:"src_port"`
+	DestPort uint16   `json:"dest_port"`
+	Protocol uint8    `json:"protocol"`
+	_        [3]byte  //padding
+}
+
+type NetPacketData struct {
+	timestamp uint64  `json:"time_stamp"`
+	comm      string  `json:"comm"`
+	hostTid   uint32  `json:"host_tid"`
+	pktLen    int     `json:"pkt_len"`
+	metaData  pktMeta `json:"meta_data"`
+}
+type DnsRequestPacketData struct {
+	netData    NetPacketData `json:"netData"`
+	query      string        `json:"query"`
+	queryType  string        `json:"queryType"`
+	queryclass string        `json:"queryclass"`
+}
+type DnsAnswerData struct {
+	number      int    `json:"number"`
+	answerType  string `json:"answer_type"`
+	answerClass string `json:"answer_class"`
+	recordName  string `json:"record_name"`
+	ttl         int    `json:"ttl"`
+}
+type DnsResponsePacketData struct {
+	netData   NetPacketData   `json:"net_data"`
+	queryData []DnsAnswerData `json:"query_data"`
+}
+
+func parsePacketMetaData(payload []byte) (pktMeta, error) {
+	var pktMetaData pktMeta
+	payloadReader := bytes.NewReader(payload)
+	err := binary.Read(payloadReader, binary.LittleEndian, &pktMetaData)
+	if err != nil {
+		fmt.Println("error binary read parsePacketMetaData \n\n")
+		return pktMetaData, err
+	}
+	return pktMetaData, nil
+}
+func parseNetPacketData(payload []byte) (NetPacketData, error) {
+	var pktData NetPacketData
+	//buf := new(bytes.Buffer)
+	//binary.Write(buf, binary.LittleEndian, &payload)
+	dataBuff := bytes.NewBuffer(payload[32:])
+
+	var pktLen uint32
+	err := binary.Read(dataBuff, binary.LittleEndian, &pktLen)
+	if err != nil {
+		fmt.Println("asdasda\n\n\n")
+	}
+	timeStamp := binary.LittleEndian.Uint64(payload[0:8])
+	hostTid := binary.LittleEndian.Uint32(payload[12:16])
+	comm := string(bytes.TrimRight(payload[16:32], "\x00"))
+	pktData.timestamp = timeStamp
+	pktData.hostTid = hostTid
+	pktData.comm = comm
+
+	pktData.pktLen = int(pktLen)
+	pktMeta, err := parsePacketMetaData(payload)
+	if err != nil {
+		return pktData, err
+	}
+	pktData.metaData = pktMeta
+	return pktData, nil
+}
+func ParseDnsRequestDomain(payload []byte) string {
 	for idx, val := range payload {
 		if int16(val) < 32 && idx != 0 {
 			payload[idx] = byte('.')
@@ -18,15 +88,11 @@ func DnsPaseName(payload []byte) string {
 	}
 	return string(payload)
 }
-
-//asumme we get the payload as the start of the name and then we parse the name, class , type
-func ParseDnsMetaData(payload []byte) ([3]string, int32) {
+func ParseDnsQuestion(payload []byte) ([3]string, int32) {
 
 	queryData := [3]string{"", "Unknown", "Unknown"} //name, type, class
 	for idx, val := range payload {
 		if val == 0 || val == 0xc0 {
-			//fmt.Println(payload[idx:idx+10])
-
 			if val == 0xc0 {
 				idx++
 			} else if payload[idx+1] == 0xc0 {
@@ -34,7 +100,7 @@ func ParseDnsMetaData(payload []byte) ([3]string, int32) {
 				queryData[0] = "prev"
 			} else {
 				if idx != 0 {
-					queryData[0] = DnsPaseName(payload[1:idx])
+					queryData[0] = ParseDnsRequestDomain(payload[1:idx])
 				}
 			}
 			dataTypeB := payload[idx+2]
@@ -75,6 +141,90 @@ func ParseDnsMetaData(payload []byte) ([3]string, int32) {
 	}
 	return queryData, 0
 }
+func parseDnsRequestPacket(payload []byte) (DnsRequestPacketData, error) {
+	var request DnsRequestPacketData
+	var err error = nil
+	p := payload
+	request.netData, err = parseNetPacketData(payload)
+	if err != nil {
+		return request, err
+	}
+	question, idx := ParseDnsQuestion(p[52:])
+	if idx == 0 {
+		return request, fmt.Errorf("Error in ParseDnsQuestion")
+	}
+	request.query = question[0]
+	request.queryType = question[1]
+	request.queryclass = question[2]
+	return request, nil
+}
+
+func DnsPaseName(payload []byte) string {
+	for idx, val := range payload {
+		if int16(val) < 32 && idx != 0 {
+			payload[idx] = byte('.')
+		}
+	}
+	return string(payload)
+}
+
+//asumme we get the payload as the start of the name and then we parse the name, class , type
+//func ParseDnsMetaData(payload []byte) ([3]string, int32) {
+//
+//	queryData := [3]string{"", "Unknown", "Unknown"} //name, type, class
+//	for idx, val := range payload {
+//		if val == 0 || val == 0xc0 {
+//			//fmt.Println(payload[idx:idx+10])
+//
+//			if val == 0xc0 {
+//				idx++
+//			} else if payload[idx+1] == 0xc0 {
+//				idx += 2
+//				queryData[0] = "prev"
+//			} else {
+//				if idx != 0 {
+//					queryData[0] = DnsPaseName(payload[1:idx])
+//				}
+//			}
+//			dataTypeB := payload[idx+2]
+//			dataClassB := payload[idx+4]
+//			switch dataClassB {
+//			case 0:
+//				queryData[2] = "Reserved"
+//			case 1:
+//				queryData[2] = "IN"
+//			case 2:
+//				queryData[2] = "Unassigned"
+//			case 3:
+//				queryData[2] = "CH"
+//			case 4:
+//				queryData[2] = "HS"
+//			}
+//			switch dataTypeB {
+//			case 1:
+//				queryData[1] = "A (IPv4)"
+//
+//			case 28:
+//				queryData[1] = "AAAA (IPv6)"
+//			case 16:
+//				queryData[1] = "TXT"
+//			case 33:
+//				queryData[1] = "SRV (location of service)"
+//			case 5:
+//				queryData[1] = "CNAME"
+//			case 15:
+//				queryData[1] = "MX"
+//			case 2:
+//				queryData[2] = "NS"
+//
+//			}
+//			return queryData, int32(idx + 4)
+//		}
+//
+//	}
+//	return queryData, 0
+//}
+
 func (t *Tracee) processNetEvents() {
 	// Todo: split pcap files by context (tid + comm)
 	// Todo: add stats for network packets (in epilog)
@@ -161,57 +311,65 @@ func (t *Tracee) processNetEvents() {
 					continue
 				}
 			} else if netEventId == NetDnsRequest {
+				//var request DnsRequestPacketData
+				//var meta pktMeta
+				//var pktLen uint32
+				//err := binary.Read(dataBuff, binary.LittleEndian, &pktLen)
+				//if err != nil {
+				//	t.handleError(err)
+				//	continue
+				//}
+				//var ifindex uint32
+				//err = binary.Read(dataBuff, binary.LittleEndian, &ifindex)
+				//if err != nil {
+				//	t.handleError(err)
+				//	continue
+				//}
+				//
+				//err = binary.Read(dataBuff, binary.LittleEndian, &meta)
+				//if err != nil {
+				//	t.handleError(err)
+				//	continue
+				//}
+				//packetMeta,e := parseNetPacketData(in)
+				//if e != nil{
+				//	fmt.Println(e)
+				//}
+				//fmt.Println("packetMeta:\n", packetMeta)
+				dnsRequest, err := parseDnsRequestPacket(in)
 
-				var pktMeta struct {
-					SrcIP    [16]byte
-					DestIP   [16]byte
-					SrcPort  uint16
-					DestPort uint16
-					Protocol uint8
-					_        [3]byte //padding
-				}
-				var pktLen uint32
-				err := binary.Read(dataBuff, binary.LittleEndian, &pktLen)
 				if err != nil {
-					t.handleError(err)
-					continue
+					fmt.Println(err)
 				}
-				var ifindex uint32
-				err = binary.Read(dataBuff, binary.LittleEndian, &ifindex)
-				if err != nil {
-					t.handleError(err)
-					continue
-				}
+				fmt.Println("request:\n\n", dnsRequest)
+				//packetMeta.metaData = meta
+				//packetMeta.pktLen = int(pktLen)
+				//packetMeta.hostTid =
+				//request.netData =
 
-				err = binary.Read(dataBuff, binary.LittleEndian, &pktMeta)
-				if err != nil {
-					t.handleError(err)
-					continue
-				}
-
-				dataBytes := dataBuff.Bytes()
-				for _ = range dataBytes {
-					if dataBytes[len(dataBytes)-1] == 0 {
-						dataBytes = dataBytes[:len(dataBytes)-1]
-					} else {
-						break
-					}
-				}
-				requestMetaDeta, _ := ParseDnsMetaData(dataBytes[len(dataBytes)-28:])
-
-				fmt.Printf("%v  %-16s  %-7d  net_events/dns_request               Len: %d, SrcIP: %v, SrcPort: %d, DestIP: %v, DestPort: %d, Protocol: %d, Query: %s, Type: %s , Class %s \n",
-					timeStampObj,
-					comm,
-					hostTid,
-					pktLen,
-					netaddr.IPFrom16(pktMeta.SrcIP),
-					pktMeta.SrcPort,
-					netaddr.IPFrom16(pktMeta.DestIP),
-					pktMeta.DestPort,
-					pktMeta.Protocol,
-					requestMetaDeta[0],
-					requestMetaDeta[1],
-					requestMetaDeta[2])
+				//dataBytes := dataBuff.Bytes()
+				//for _ = range dataBytes {
+				//	if dataBytes[len(dataBytes)-1] == 0 {
+				//		dataBytes = dataBytes[:len(dataBytes)-1]
+				//	} else {
+				//		break
+				//	}
+				//}
+				//requestMetaDeta, _ := ParseDnsMetaData(dataBytes[len(dataBytes)-28:])
+				//
+				//fmt.Printf("%v  %-16s  %-7d  net_events/dns_request               Len: %d, SrcIP: %v, SrcPort: %d, DestIP: %v, DestPort: %d, Protocol: %d, Query: %s, Type: %s , Class %s \n",
+				//	timeStampObj,
+				//	comm,
+				//	hostTid,
+				//	pktLen,
+				//	netaddr.IPFrom16(pktMeta.SrcIP),
+				//	pktMeta.SrcPort,
+				//	netaddr.IPFrom16(pktMeta.DestIP),
+				//	pktMeta.DestPort,
+				//	pktMeta.Protocol,
+				//	requestMetaDeta[0],
+				//	requestMetaDeta[1],
+				//	requestMetaDeta[2])
 
 			} else if netEventId == NetDnsResponse {
 				var pktMeta struct {
@@ -241,94 +399,94 @@ func (t *Tracee) processNetEvents() {
 					continue
 				}
 
-				dataBytes := dataBuff.Bytes()
-				for _ = range dataBytes {
-					if dataBytes[len(dataBytes)-1] == 0 {
-						dataBytes = dataBytes[:len(dataBytes)-1]
-					} else {
-						break
-					}
-				}
-				ansNumber := int(dataBytes[49])
-				//parse query metadata
-				queryData, offset := ParseDnsMetaData(dataBytes[54:])
-
-				offset += 54
-				//loop over the response answers
-				for i := 0; i < ansNumber; i++ {
-					ansMetaData, ansOffset := ParseDnsMetaData(dataBytes[offset:])
-					offset += ansOffset
-					if ansMetaData[0] == "prev" {
-						ansMetaData[0] = queryData[0]
-					}
-					TTL := int32(binary.BigEndian.Uint32(dataBytes[offset+1 : offset+5]))
-					fmt.Printf("ttl is %d\n", TTL)
-
-					dataLen := binary.BigEndian.Uint16(dataBytes[offset+5 : offset+7]) // binary.LittleEndian.Uint16(dataBytes[offset+8:offset+10]) //dataBytes[offset+4]
-
-					addr := netaddr.IP{}
-					nameRecord := ""
-					switch ansMetaData[1] {
-					case "CNAME":
-						nameRecord = string(dataBytes[offset+7 : offset+7+(int32(dataLen))])
-						//fmt.Printf("name record is %s\n", nameRecod)
-					case "A (IPv4)":
-						responseAddr := [4]byte{0}
-						copy(responseAddr[:], dataBytes[offset+7:offset+11])
-						addr = netaddr.IPFrom4(responseAddr)
-						//fmt.Printf("IPv4 is %v\n", addr)
-					case "AAAA (IPv6)":
-						responseAddr := [16]byte{0}
-						copy(responseAddr[:], dataBytes[offset+7:offset+23])
-						addr = netaddr.IPFrom16(responseAddr)
-						//fmt.Printf("IPv6 is %v\n", addr)
-					}
-					if ansMetaData[1] == "CNAME" {
-						fmt.Printf("%v  %-16s  %-7d  net_event/dns_response               Len: %d, SrcIP: %v, SrcPort: %d, DestIP: %v, DestPort: %d, Protocol: %d, Query: %s, Type: %s , Class: %s, Answer[%d]: Name:%v, Type: %v, Class: %v, record_name: %s, TTL: %v\n",
-							timeStampObj,
-							comm,
-							hostTid,
-							pktLen,
-							netaddr.IPFrom16(pktMeta.SrcIP),
-							pktMeta.SrcPort,
-							netaddr.IPFrom16(pktMeta.DestIP),
-							pktMeta.DestPort,
-							pktMeta.Protocol,
-							queryData[0],
-							queryData[1],
-							queryData[2],
-							i+1,
-							ansMetaData[0],
-							ansMetaData[1],
-							ansMetaData[2],
-							nameRecord,
-							TTL)
-						//fmt.Printf("dns_response: Name:%v, Type: %v, Class: %v, Address: %v, TTL: %v, addr: %-16s\n",ansMetaData[0],ansMetaData[1],ansMetaData[2],0,TTL, nameRecord )
-
-					} else {
-						fmt.Printf("%v  %-16s  %-7d  net_event/dns_response               Len: %d, SrcIP: %v, SrcPort: %d, DestIP: %v, DestPort: %d, Protocol: %d, Query: %s, Type: %s , Class: %s, Answer[%d]: Name:%v, Type: %v, Class: %v, Address: %-16s, TTL: %v\n",
-							timeStampObj,
-							comm,
-							hostTid,
-							pktLen,
-							netaddr.IPFrom16(pktMeta.SrcIP),
-							pktMeta.SrcPort,
-							netaddr.IPFrom16(pktMeta.DestIP),
-							pktMeta.DestPort,
-							pktMeta.Protocol,
-							queryData[0],
-							queryData[1],
-							queryData[2],
-							i+1,
-							ansMetaData[0],
-							ansMetaData[1],
-							ansMetaData[2],
-							addr,
-							TTL)
-					}
-					offset += int32(dataLen + 7)
-
-				}
+				////dataBytes := dataBuff.Bytes()
+				////for _ = range dataBytes {
+				////	if dataBytes[len(dataBytes)-1] == 0 {
+				////		dataBytes = dataBytes[:len(dataBytes)-1]
+				////	} else {
+				////		break
+				////	}
+				////}
+				////ansNumber := int(dataBytes[49])
+				//////parse query metadata
+				////queryData, offset := ParseDnsMetaData(dataBytes[54:])
+				////
+				////offset += 54
+				//////loop over the response answers
+				////for i := 0; i < ansNumber; i++ {
+				////	ansMetaData, ansOffset := ParseDnsMetaData(dataBytes[offset:])
+				////	offset += ansOffset
+				////	if ansMetaData[0] == "prev" {
+				////		ansMetaData[0] = queryData[0]
+				////	}
+				////	TTL := int32(binary.BigEndian.Uint32(dataBytes[offset+1 : offset+5]))
+				////	fmt.Printf("ttl is %d\n", TTL)
+				////
+				////	dataLen := binary.BigEndian.Uint16(dataBytes[offset+5 : offset+7]) // binary.LittleEndian.Uint16(dataBytes[offset+8:offset+10]) //dataBytes[offset+4]
+				////
+				////	addr := netaddr.IP{}
+				////	nameRecord := ""
+				////	switch ansMetaData[1] {
+				////	case "CNAME":
+				////		nameRecord = string(dataBytes[offset+7 : offset+7+(int32(dataLen))])
+				////		//fmt.Printf("name record is %s\n", nameRecod)
+				////	case "A (IPv4)":
+				////		responseAddr := [4]byte{0}
+				////		copy(responseAddr[:], dataBytes[offset+7:offset+11])
+				////		addr = netaddr.IPFrom4(responseAddr)
+				////		//fmt.Printf("IPv4 is %v\n", addr)
+				////	case "AAAA (IPv6)":
+				////		responseAddr := [16]byte{0}
+				////		copy(responseAddr[:], dataBytes[offset+7:offset+23])
+				////		addr = netaddr.IPFrom16(responseAddr)
+				////		//fmt.Printf("IPv6 is %v\n", addr)
+				////	}
+				////	if ansMetaData[1] == "CNAME" {
+				////		fmt.Printf("%v  %-16s  %-7d  net_event/dns_response               Len: %d, SrcIP: %v, SrcPort: %d, DestIP: %v, DestPort: %d, Protocol: %d, Query: %s, Type: %s , Class: %s, Answer[%d]: Name:%v, Type: %v, Class: %v, record_name: %s, TTL: %v\n",
+				////			timeStampObj,
+				////			comm,
+				////			hostTid,
+				////			pktLen,
+				////			netaddr.IPFrom16(pktMeta.SrcIP),
+				////			pktMeta.SrcPort,
+				////			netaddr.IPFrom16(pktMeta.DestIP),
+				////			pktMeta.DestPort,
+				////			pktMeta.Protocol,
+				////			queryData[0],
+				////			queryData[1],
+				////			queryData[2],
+				////			i+1,
+				////			ansMetaData[0],
+				////			ansMetaData[1],
+				////			ansMetaData[2],
+				////			nameRecord,
+				////			TTL)
+				////		//fmt.Printf("dns_response: Name:%v, Type: %v, Class: %v, Address: %v, TTL: %v, addr: %-16s\n",ansMetaData[0],ansMetaData[1],ansMetaData[2],0,TTL, nameRecord )
+				////
+				////	} else {
+				////		fmt.Printf("%v  %-16s  %-7d  net_event/dns_response               Len: %d, SrcIP: %v, SrcPort: %d, DestIP: %v, DestPort: %d, Protocol: %d, Query: %s, Type: %s , Class: %s, Answer[%d]: Name:%v, Type: %v, Class: %v, Address: %-16s, TTL: %v\n",
+				////			timeStampObj,
+				////			comm,
+				////			hostTid,
+				////			pktLen,
+				////			netaddr.IPFrom16(pktMeta.SrcIP),
+				////			pktMeta.SrcPort,
+				////			netaddr.IPFrom16(pktMeta.DestIP),
+				////			pktMeta.DestPort,
+				////			pktMeta.Protocol,
+				////			queryData[0],
+				////			queryData[1],
+				////			queryData[2],
+				////			i+1,
+				////			ansMetaData[0],
+				////			ansMetaData[1],
+				////			ansMetaData[2],
+				////			addr,
+				////			TTL)
+				////	}
+				////	offset += int32(dataLen + 7)
+				//
+				//}
 
 			} else if t.config.Debug {
 				var pkt struct {
